10eda06cd73dcf97c86b203ebe48f1c195c5a98d
Brandon Nolan <brndalx@gmail.com>
2023-02-20 00:50:42 +0200

    feat: pegenation for GET

--- routes/movies.js
+++ routes/movies.js
@@ -2,8 +2,20 @@ const express = require("express");
 const { MovieModel } = require("../models/movieModel");
 const router = express.Router();
 router.get("/", async (req, res) => {
+  //pegenation variabels
+  let perPage = 5;
+  let page = req.query.page - 1 || 0;
+  let sort = req.query.sort || "name";
+  let desc = req.query.desc == "yes" ? 1 : -1;
+
   try {
-    let data = await MovieModel.find({});
+    let data = await MovieModel.find({})
+      .limit(perPage)
+      .skip(page * perPage)
+      .sort({ [sort]: desc });
+    if (data.length == 0) {
+      return res.json({ msg: "No such page" });
+    }
     res.json(data);
   } catch (err) {
     console.log(err);
