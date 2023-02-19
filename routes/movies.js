e5d2be1f2df78a6319952305c7a2c0e371a56f43
Brandon Nolan <brndalx@gmail.com>
2023-02-19 19:02:55 +0200

    feat: get request to db

--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
 const router = express.Router();
 router.get("/", async (req, res) => {
-  res.json({ msg: "this is movies route" });
+  try {
+    const client = await MongoClient.connect("mongodb://localhost:27017", {
+      useUnifiedTopology: true,
+    });
+    const db = client.db("server3");
+    const collection = db.collection("movies");
+    const data = await collection.find({}).limit(20).toArray();
+    client.close();
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(502).json({ err });
+  }
 });
 module.exports = router;
