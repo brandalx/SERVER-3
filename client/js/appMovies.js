5d5f27f5cb2f78eeb91369e6f42e8f334a890e9c
Brandon Nolan <brndalx@gmail.com>
2023-02-20 03:23:42 +0200

    feat: pegenation by button clicks

--- client/js/appMovies.js
+++ client/js/appMovies.js
@@ -1,17 +1,42 @@
 import MoviesItem from "./moviesItem.js";
-
+let query = 1;
 const init = () => {
   doApi();
+  declareEvemts();
+};
+let back = document.querySelector("#btnBack");
+let next = document.querySelector("#btnNext");
+const declareEvemts = () => {
+  next.addEventListener("click", () => {
+    query++;
+    if (query > 4) {
+      query = 4;
+      return 0;
+    } else {
+      doApi(query);
+    }
+  });
+  back.addEventListener("click", () => {
+    query--;
+    if (query < 1) {
+      query = 1;
+      return 0;
+    } else {
+      doApi(query);
+    }
+  });
 };
 
-const doApi = async () => {
-  let url = "http://localhost:3003/movies";
+const doApi = async (peg) => {
+  let url = `http://localhost:3003/movies?page=${peg}`;
   let resp = await axios.get(url);
   console.log(resp.data);
   createList(resp.data);
 };
 
 const createList = (_ar) => {
+  let mainAppender = document.querySelector("#id_parent");
+  mainAppender.innerHTML = " ";
   _ar.forEach((item, i) => {
     let movies = new MoviesItem(item, i);
     movies.render();
