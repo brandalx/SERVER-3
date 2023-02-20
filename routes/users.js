acad7bd56827e19be7ca7d0014ad48c3a638f5f2
Brandon Nolan <brndalx@gmail.com>
2023-02-20 02:21:15 +0200

    feat: POST for user data, login for user - token

--- routes/users.js
+++ routes/users.js
@@ -13,9 +13,20 @@ router.get("/", async (req, res) => {
   res.json({ msg: "this is users endpoint" });
 });
 
+function generateRandomStars() {
+  const numStars = Math.floor(Math.random() * (1 - 10 + 1)) + 1;
+  return "*".repeat(numStars);
+}
+
 router.get("/userInfo", async (req, res) => {
   try {
-    res.json({ msg: "this is users info" });
+    let user = await UserModel.findOne(
+      {
+        _id: req.tokenData._id,
+      },
+      { password: 0 }
+    );
+    res.json(user);
   } catch (err) {
     console.log(err);
     res.status(502).json({ err });
@@ -23,8 +34,16 @@ router.get("/userInfo", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
+  let validBody = validateJoi(req.body);
+  if (validBody.error) {
+    return res.status(400).json(validBody.error.details);
+  }
   try {
-    res.json({ msg: "this is users info" });
+    let user = new UserModel(req.body);
+    user.password = await bcrypt.hash(user.password, 10);
+    await user.save();
+    user.password = generateRandomStars;
+    res.status(201).json(user);
   } catch (err) {
     console.log(err);
     res.status(502).json({ err });
@@ -32,8 +51,21 @@ router.post("/", async (req, res) => {
 });
 
 router.post("/login", async (req, res) => {
+  let validBody = validateLogin(req.body);
+  if (validBody.error) {
+    return res.status(400).json(validBody.error.details);
+  }
   try {
-    res.json({ msg: "this is users info" });
+    let user = await UserModel.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(401).json({ err: "Email you provided not found" });
+    }
+    let validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) {
+      return res.status(401).json({ err: "Password you provided is wrong" });
+    }
+    let token = createToken(user._id);
+    return res.json({ token });
   } catch (err) {
     console.log(err);
     res.status(502).json({ err });
